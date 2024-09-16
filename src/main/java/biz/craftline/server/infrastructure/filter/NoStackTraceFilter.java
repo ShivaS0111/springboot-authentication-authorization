176b367d7027d56731037b0590547ee323969aac
shivaprasad <shivaprasad.andy@gmail.com>
2024-09-16 22:32:53 +0530

    docker working fine with cl and sql containers

--- src/main/java/biz/craftline/server/infrastructure/filter/NoStackTraceFilter.java
+++ src/main/java/biz/craftline/server/infrastructure/filter/NoStackTraceFilter.java
@@ -3,8 +3,9 @@ package biz.craftline.server.infrastructure.filter;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.filter.Filter;
 import ch.qos.logback.core.spi.FilterReply;
+import org.springframework.stereotype.Component;
 
-
+@Component
 public class NoStackTraceFilter extends Filter<ILoggingEvent> {
     @Override
     public FilterReply decide(ILoggingEvent event) {
